e3b870a90d41dddd69192ddd6e2b847c2e56b325
JTGlez <jorjeluigi1@comunidad.unam.mx>
2024-02-11 21:04:35 -0600

    Environment Variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,21 +5,21 @@ import { getFirestore } from 'firebase/firestore/lite'
 import { getEnvironments } from "../helpers";
 
 const {
-    VITE_API_KEY,
-    VITE_AUTH_DOMAIN,
-    VITE_PROJECT_ID,
-    VITE_STORAGE_BUCKET,
-    VITE_MESSAGING_SENDER_ID,
-    VITE_APP_ID
+    NEXT_PUBLIC_VITE_API_KEY,
+    NEXT_PUBLIC_VITE_AUTH_DOMAIN,
+    NEXT_PUBLIC_VITE_PROJECT_ID,
+    NEXT_PUBLIC_VITE_STORAGE_BUCKET,
+    NEXT_PUBLIC_VITE_MESSAGING_SENDER_ID,
+    NEXT_PUBLIC_VITE_APP_ID
 } = getEnvironments();
 
 const firebaseConfig = {
-    apiKey: VITE_API_KEY,
-    authDomain: VITE_AUTH_DOMAIN,
-    projectId: VITE_PROJECT_ID,
-    storageBucket: VITE_STORAGE_BUCKET,
-    messagingSenderId: VITE_MESSAGING_SENDER_ID,
-    appId: VITE_APP_ID
+    apiKey: NEXT_PUBLIC_VITE_API_KEY,
+    authDomain: NEXT_PUBLIC_VITE_AUTH_DOMAIN,
+    projectId: NEXT_PUBLIC_VITE_PROJECT_ID,
+    storageBucket: NEXT_PUBLIC_VITE_STORAGE_BUCKET,
+    messagingSenderId: NEXT_PUBLIC_VITE_MESSAGING_SENDER_ID,
+    appId: NEXT_PUBLIC_VITE_APP_ID
 };
 
 // Initialize Firebase
