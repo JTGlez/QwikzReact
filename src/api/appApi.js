bed714e42e687f04b7003e60f19d05681c01f526
JTGlez <jorjeluigi1@comunidad.unam.mx>
2024-02-14 20:51:02 -0600

    Debug endpoint

--- src/api/appApi.js
+++ src/api/appApi.js
@@ -4,6 +4,8 @@ import { getEnvironments } from '../helpers'
 
 const { VITE_API_URL, VITE_API_URL_TEST } = getEnvironments();
 
+console.log(VITE_API_URL)
+
 const appApi = axios.create({
     baseURL: VITE_API_URL
 })
