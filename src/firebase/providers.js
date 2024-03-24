98e2d127c297089e857f507a12b60aa3a543a68e
JTGlez <jorjeluigi1@comunidad.unam.mx>
2024-03-23 22:30:15 -0600

    Flask test response

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -29,28 +29,30 @@ export const registerUserWithEmailPassword = async ({ displayName, email, passwo
         const token = await getCurrentUserToken();
 
         // Calls the Flask-Axios backend to register the user and set their custom claims using the token
-/*         const flaskResp = await api.post('/auth/register', {
+        const flaskResp = await api.post('/auth/register', {
             accountType
         }, {
             headers: {
                 'Authorization': `Bearer ${token.token}`,
                 'Content-Type': 'application/json',
             }
-        }); */
+        });
+
+        console.log(flaskResp)
 
         // Get the custom token from the Flask backend
         // const customToken = flaskResp.data.customToken;
 
         // Sign in with the custom token from the Flask backend (Using a nested try-catch block)
-/*         try {
-
-            const resp = await signInWithCustomToken(firebaseAuth, customToken);
-            // After creating the user and signing in, we can update his profile.
-            await updateProfile(firebaseAuth.currentUser, { displayName });
-
-        } catch (error) {
-            console.error('Error signing in with custom token:', error);
-        } */
+        /*         try {
+        
+                    const resp = await signInWithCustomToken(firebaseAuth, customToken);
+                    // After creating the user and signing in, we can update his profile.
+                    await updateProfile(firebaseAuth.currentUser, { displayName });
+        
+                } catch (error) {
+                    console.error('Error signing in with custom token:', error);
+                } */
 
         return {
             ok: true,
