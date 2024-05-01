b890f508da4ffe74da0ad9e31aea7621026a278b
LuisMPS <luismario14@gmail.com>
2024-05-01 13:15:00 -0600

    Despliego componente para crear quizzes temporalmente en NoActiveGroupView de teachers

--- src/teachers/views/NoActiveGroupView.jsx
+++ src/teachers/views/NoActiveGroupView.jsx
@@ -1,21 +1,53 @@
 /* eslint-disable no-unused-vars */
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
 // import { GroupCard } from "../components/GroupCard/GroupCard"
 import GroupCard from "@/components/ui/group-card";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button";
+import AddQuiz from "../components/AddQuiz";
 
 export const NoActiveGroupView = () => {
 
     const dispatch = useDispatch();
     const { groups } = useSelector(state => state.teachers);
 
+    const [quizDialogOpen, setQuizDialogOpen] = useState(false);
+
+    const onQuizSubmitted = () => {
+        // dispatch();
+        setQuizDialogOpen(false);
+    }
+
     if (!groups.length) {
-        return (
-            <div className="flex justify-center items-center h-[80vh]">
-                <p className="text-lg font-semibold text-center text-gray-400">
-                    Create an active group to start teaching!
-                </p>
+        return (<div className="flex justify-center items-center h-[80vh] flex-col gap-2">
+            <p className="text-lg font-semibold text-center text-gray-400">
+                Create an active group to start teaching!
+            </p>
+            <div className="text-lg font-semibold text-center text-gray-400">
+                <Dialog open={quizDialogOpen} onOpenChange={setQuizDialogOpen}>
+                    <DialogTrigger asChild>
+                        <Button variant="outline">Agrega cuestionarios</Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[60rem]">
+                        <DialogHeader>
+                            <DialogTitle>Nuevo Cuestionario</DialogTitle>
+                            <DialogDescription>
+                                A continuación puede crear un nuevo cuestionario
+                            </DialogDescription>
+                            <AddQuiz onQuizSubmitted={onQuizSubmitted}/>
+                        </DialogHeader>
+                    </DialogContent>
+                </Dialog>
             </div>
-        )
+        </div>)
     }
 
     return (
