f531c10e70dbe2df98658522e38ea7d4c2c5d007
LuisMPS <luismario14@gmail.com>
2024-05-14 10:53:11 -0600

    Muevo ruta de quizzes de auth a students

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -2,7 +2,6 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { LoginPage } from "../pages";
 import { HomePage } from "../pages";
 import { RegisterPage } from "../pages/RegisterPage";
-import { QuizzPage } from "@/components/ui/quizz";
 
 export const AuthRoutes = () => {
     return (
@@ -11,7 +10,6 @@ export const AuthRoutes = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="login" element={<LoginPage />} />
             <Route path="register" element={<RegisterPage />} />
-            <Route path="quiz/:quizID" element={<QuizzPage />} />
             <Route path="/*" element={<Navigate to="/auth/login" />} />
         </Routes>
     )
